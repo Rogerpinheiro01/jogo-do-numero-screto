a9164c29af0940841e659478d428d76de342279d
Rogerpinheiro01 <roger.developer001@gmail.com>
2024-03-06 13:24:28 -0300

    Alterado de 10 para 100

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Número Secreto que foi Puxado da Função 'gerarNumeroAleatorio'
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag,texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1','Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
     exibirMensagemInicial();
 
